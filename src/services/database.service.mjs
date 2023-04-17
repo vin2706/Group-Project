115b0cbefd5597958188df31ca53031c078d5e05
Cameron Kerwick <kerwickc@roehmapton.ac.uk>
2023-04-17 14:41:47 +0100

    sprint 4 code

--- src/services/database.service.mjs
+++ src/services/database.service.mjs
@@ -73,10 +73,51 @@ export default class DatabaseService {
   }
 
   /* Get a list of countries */
-  async getCountries() {
-    const sql = `SELECT * FROM country`;
-    const [rows, fields] = await this.conn.execute(sql);
-    const countries = rows.map(c => new Country(c.Code, c.Name, c.Continent, c.Region, c.Population));
-    return countries;
+/* Get a list of all countries */
+async getCountries() {
+  try {
+    // Fetch countries from database
+    const data = await this.conn.execute("SELECT * FROM `country`");
+    return data;
+  } catch (err) {
+    // Handle error...
+    console.error(err);
+    return undefined;
   }
+}
+
+/* Get a particular country by code, including cities information */
+async getCountry(countryCode) {
+  const sql = `
+      SELECT country.*, city.ID as CityID, city.Name as CityName, city.District, city.Population as CityPopulation
+      FROM country
+      INNER JOIN city ON city.CountryCode = country.Code
+      WHERE country.Code = "${countryCode}"
+  `;
+  const [rows, fields] = await this.conn.execute(sql);
+  /* Get the first result of the query (we're looking up the country by code, which should be unique) */
+  const data = rows[0];
+  const country = new Country(
+    data.Code,
+    data.Name,
+    data.Continent,
+    data.Region,
+    data.Population
+  );
+  const cities = [];
+  rows.forEach(row => {
+    const city = new City(
+      row.CityID,
+      row.CityName,
+      row.CountryCode,
+      row.District,
+      row.CityPopulation
+    );
+    cities.push(city);
+  });
+  country.cities = cities;
+  return country;
+}
+ 
+  
 }
