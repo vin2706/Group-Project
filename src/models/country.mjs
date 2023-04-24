d87b6f1588a856c09527ea5632490869810291b5
Vindya Ashok <ashokv@roehampton.ac.uk>
2023-04-24 13:42:48 +0100

    Sprint 4 Code

--- src/models/country.mjs
+++ src/models/country.mjs
@@ -4,12 +4,14 @@ export default class Country {
     continent;
     region;
     population;
+    capital;
   
-    constructor(code, name, continent, region, population) {
+    constructor(code, name, continent, region, population, capital) {
       this.code = code;
       this.name = name;
       this.continent = continent;
       this.region = region;
       this.population = population;
+      this.capital = capital;
     }
   }
