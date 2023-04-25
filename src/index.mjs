98950f84e3c8484d2dfd38093b6cc09f4579381a
Vindya Ashok <ashokv@roehampton.ac.uk>
2023-04-25 18:07:42 +0100

    Sprint 4

--- src/index.mjs
+++ src/index.mjs
@@ -164,18 +164,75 @@ app.get("/api/countries", async (req, res) => {
   return res.send(rows);
 });
 
-app.get('/capital-city-report', async (req, res) => {
-  const [rows, fields] = await db.conn.execute(`
-    SELECT c.Name AS City, co.Name AS Country, co.Continent AS Continent, co.Region AS Region, c.Population
-    FROM city c
-    JOIN country co ON c.CountryCode = co.Code
-    WHERE c.ID = Capital
-    ORDER BY co.Name ASC
-  `);
 
-  res.render('capital-city-report', { rows });
+app.get('/capital-city-report', async (req, res) => {
+  let sortBy;
+  switch (req.query['sort-by']) {
+    case 'name':
+      sortBy = 'City ASC';
+      break;
+    case 'population':
+      sortBy = 'Population DESC';
+      break;
+    case 'continent':
+      sortBy = 'Continent ASC, Population DESC';
+      break;
+    case 'region':
+      sortBy = 'Region ASC, Population DESC';
+      break;
+    default:
+      sortBy = 'City ASC';
+  }
+
+  try {
+    const [rows, fields] = await db.conn.execute(`
+      SELECT c.Name AS City, co.Name AS Country, co.Continent AS Continent, co.Region AS Region, c.Population
+      FROM city c
+      JOIN country co ON c.CountryCode = co.Code
+      WHERE c.ID = co.Capital
+      ORDER BY ${sortBy}
+    `);
+    res.render('capital-city-report', { rows, fields, sortBy });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
+app.post('/capital-city-report', async (req, res) => {
+  const { continent } = req.body;
+  let sortBy;
+  switch (req.query['sort-by']) {
+    case 'name':
+      sortBy = 'City ASC';
+      break;
+    case 'population':
+      sortBy = 'Population DESC';
+      break;
+    case 'continent':
+      sortBy = 'Continent ASC, Population DESC';
+      break;
+    case 'region':
+      sortBy = 'Region ASC, Population DESC';
+      break;
+    default:
+      sortBy = 'City ASC';
+  }
+
+  try {
+    const [rows, fields] = await db.conn.execute(`
+      SELECT c.Name AS City, co.Name AS Country, co.Continent AS Continent, co.Region AS Region, c.Population
+      FROM city c
+      JOIN country co ON c.CountryCode = co.Code
+      WHERE c.ID = co.Capital AND co.Continent = '${continent}'
+      ORDER BY ${sortBy}
+    `);
+    res.render('capital-city-report', { rows, fields, sortBy });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
 // Run server!
